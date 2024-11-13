49529bd9c441b2e020bbf3c17f266c99dc4a4cd5
J2CServices <julian.jaramilo@uniminuto.edu.co>
2024-11-13 13:10:40 -0500

    tercero

--- server.js
+++ server.js
@@ -4,38 +4,39 @@ const app = express();
 const PORT = process.env.PORT || 3001; // Usa el puerto de entorno para Azure
 
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static('public')); // Sirve los archivos en la carpeta 'public'
 
 // Puntos estandarizados de los límites
 const limitsPoints = [
-  { x: 125, y: 4600 },
-  { x: 127, y: 5100 },
-  { x: 130, y: 5100 },
-  { x: 135, y: 4600 },
-  { x: 137, y: 4100 },
-  { x: 125, y: 4100 }
+    { x: 125, y: 4600 },
+    { x: 127, y: 5100 },
+    { x: 130, y: 5100 },
+    { x: 135, y: 4600 },
+    { x: 137, y: 4100 },
+    { x: 125, y: 4100 }
 ];
 
 let cgPoints = []; // Array para los puntos del centro de gravedad
 
 // Endpoint para recibir solo el punto del centro de gravedad (CG)
 app.post('/add-point', (req, res) => {
-  const { x, y } = req.body;
+    const { x, y } = req.body;
 
-  if (x && y) {
-    cgPoints = [{ x, y }]; // Actualiza el punto CG
-    res.sendStatus(200);
-  } else {
-    res.sendStatus(400);
-  }
+    if (x && y) {
+        cgPoints = [{ x, y }]; // Actualiza el punto CG
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(400);
+    }
 });
 
 // Endpoint para enviar los puntos al cliente
 app.get('/data-points', (req, res) => {
-  console.log("Datos enviados al cliente:", { limits: limitsPoints, cg: cgPoints });
-  res.json({ limits: limitsPoints, cg: cgPoints });
+    console.log("Datos enviados al cliente:", { limits: limitsPoints, cg: cgPoints });
+    res.json({ limits: limitsPoints, cg: cgPoints });
 });
 
+// Inicia el servidor en el puerto especificado
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
